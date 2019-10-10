d1f5e2240fd5b8d291f8d9f581f014e4354274ea
hy21898 <hy21898@gmail.com>
2019-10-10 11:17:25 +0700

    sinh vien D sua 7

--- source.cpp
+++ source.cpp
@@ -22,3 +22,18 @@ void Nam()
 	cout<<"Hello eeverybodyhandsome";
 
 }
+void tAnh()
+{
+	cout<<"toi la nguoi xau nhat nhom Tuan Anh ";
+}
+int BSCLN(int a, int b)
+{
+	while (a!=b)
+	{
+		if(a<b)
+			a-=b;
+		else
+			b-=a;
+	}
+	return a;
+}
